ec81eb039c0b35a44968ee55157b3189911086ae
wvit <1999wuwei@gmail.com>
2018-07-13 07:36:33 +0800

    调整代码

--- mpvue-project/src/App.vue
+++ mpvue-project/src/App.vue
@@ -3,14 +3,8 @@
 import { get } from './util';
 export default {
   async created() {
-    // wx.request({
-    //   url: config.host + '/weapp/demo',
-    //   success(res) {
-    //     console.log(res);
-    //   }
-    // });
     const res = await get('/weapp/demo');
-    console.log(123,res);
+    console.log(res);
     console.log('已经启动');
   }
 };
