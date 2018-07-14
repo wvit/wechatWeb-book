a5060aaf00620c5c544c1abd94e58bf6bd18abfc
wvit <1999wuwei@gmail.com>
2018-07-14 08:25:34 +0800

    login

--- mpvue-project/src/App.vue
+++ mpvue-project/src/App.vue
@@ -1,10 +1,22 @@
 
 <script>
 import { get } from './util';
+import qcloud from 'wafer2-client-sdk';
+import config from './config';
 export default {
-  async created() {
-    const res = await get('/weapp/demo');
-    console.log(res);
+  created() {
+    // qcloud.setLoginUrl(config.loginUrl);
+
+    // qcloud.login({
+    //   success: function(userInfo) {
+    //     console.log('登录成功', userInfo);
+    //   },
+    //   fail: function(err) {
+    //     console.log('登录失败', err);
+    //   }
+    // });
+    // const res = await get('/weapp/demo');
+    // console.log(res);
     console.log('已经启动');
   }
 };
