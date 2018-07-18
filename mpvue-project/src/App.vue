78329d2dbee88df2f793d57eff74fcf02b40425d
wvit <1999wuwei@gmail.com>
2018-07-18 08:14:05 +0800

    add

--- mpvue-project/src/App.vue
+++ mpvue-project/src/App.vue
@@ -1,29 +1,39 @@
 
 <script>
 import { get } from './util';
+import qcloud from 'wafer2-client-sdk';
 import config from './config';
 export default {
   async created() {
-       wx.login({
-        success: function(res) {
-          wx.request({
-            url: 'https://api.weixin.qq.com/sns/jscode2session',
-            data:{
-              appid:config.appid,
-              secret:config.appSecret,
-              js_code:res.code,
-              grant_type:'authorization_code'
-            },
-            header: {
-                'content-type': 'application/json'
-            },
-            success: function(res) {
-            let openid = res.data.openid //返回openid
-            console.log('openid:'+openid)
-            }
-          })
-        }
-    })
+    // qcloud.setLoginUrl(config.loginUrl);
+    // qcloud.login({
+    //   success: function(userInfo) {
+    //     console.log('登录成功', userInfo);
+    //   },
+    //   fail: function(err) {
+    //     console.log('登录失败', err);
+    //   }
+    // });
+    wx.login({
+      success: function(res) {
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+            appid: config.appid,
+            secret: config.appSecret,
+            js_code: res.code,
+            grant_type: 'authorization_code'
+          },
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function(res) {
+            let openid = res.data.openid; //返回openid
+            console.log('openid:' + openid);
+          }
+        });
+      }
+    });
     console.log('已经启动');
   }
 };
--- mpvue-project/src/pages/me/Me.vue
+++ mpvue-project/src/pages/me/Me.vue
@@ -1,22 +1,34 @@
 
 <template>
-    <div>
-        <img v-show="userInfo" :src="userInfo.avatarUrl" alt="logo">
-        <p v-show="userInfo">{{userInfo.nickName}}</p>
-        <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="userLogin">登录</button>
-    </div>
+  <div>
+    <img v-show="userInfo" :src="userInfo.avatarUrl" alt="logo">
+    <p v-show="userInfo">{{userInfo.nickName}}</p>
+    <button open-type="getUserInfo" lang="zh_CN" @getuserinfo="userLogin">登录</button>
+  </div>
 </template>
 <script>
+import { get, showSuccess } from '@/util';
 export default {
   data() {
     return {
       userInfo: {}
     };
   },
+  created() {
+    let userState = wx.getStorageSync('userInfo');
+    if (userState) {
+      this.userInfo = userState;
+    }
+  },
+
   methods: {
     userLogin(res) {
-      console.log(res);
-      this.userInfo = res.target.userInfo;
+      let userState = wx.getStorageSync('userInfo');
+      if (!userState) {
+        showSuccess('登录成功');
+        this.userInfo = res.target.userInfo;
+        wx.setStorageSync('userInfo', res.target.userInfo);
+      }
     }
   }
 };
